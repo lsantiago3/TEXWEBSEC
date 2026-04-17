c5e86a46862e4ffe308e84ebc41223f075a2a7f6
lsantiago3 <36929397+lsantiago3@users.noreply.github.com>
2026-04-17 12:22:51 -0400

    Implement CRUD operations in businessController
    Added CRUD operations for Business entities in the controller.

--- backend/controllers/businessController.js
+++ backend/controllers/businessController.js
@@ -1,1 +1,46 @@
+/**
+ * Business Controller
+ * Handles all CRUD operations for Business entities.
+ * This is the business logic layer.
+ */
+
+exports.getAllBusinesses = async (req, res, next) => {
+    try {
+        res.status(200).json({ message: "Get all businesses" });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.getBusinessById = async (req, res, next) => {
+    try {
+        res.status(200).json({ message: `Get business with ID ${req.params.id}` });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.createBusiness = async (req, res, next) => {
+    try {
+        res.status(201).json({ message: "Create a new business" });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.updateBusiness = async (req, res, next) => {
+    try {
+        res.status(200).json({ message: `Update business with ID ${req.params.id}` });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.deleteBusiness = async (req, res, next) => {
+    try {
+        res.status(200).json({ message: `Delete business with ID ${req.params.id}` });
+    } catch (error) {
+        next(error);
+    }
+};
 
