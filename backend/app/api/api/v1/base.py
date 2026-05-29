fb1c3215bc046ba9ea1882a4aa214782f897732f
lsantiago3 <36929397+lsantiago3@users.noreply.github.com>
2026-05-29 11:37:40 -0400

    Delete base.py and its router configurations
    from fastapi import APIRouter
    from .user import router as user_router
    from .business import router as business_router
    from .scan import router as scan_router
    from .report import router as report_router
    
    api_router = APIRouter()
    
    api_router.include_router(user_router, prefix="/user", tags=["User"])
    api_router.include_router(business_router, prefix="/business", tags=["Business"])
    api_router.include_router(scan_router, prefix="/scan", tags=["Scan"])
    api_router.include_router(report_router, prefix="/report", tags=["Report"])

--- backend/app/api/api/v1/base.py
+++ backend/app/api/api/v1/base.py
@@ -1,12 +1,1 @@
-from fastapi import APIRouter
-from .user import router as user_router
-from .business import router as business_router
-from .scan import router as scan_router
-from .report import router as report_router
 
-api_router = APIRouter()
-
-api_router.include_router(user_router, prefix="/user", tags=["User"])
-api_router.include_router(business_router, prefix="/business", tags=["Business"])
-api_router.include_router(scan_router, prefix="/scan", tags=["Scan"])
-api_router.include_router(report_router, prefix="/report", tags=["Report"])
