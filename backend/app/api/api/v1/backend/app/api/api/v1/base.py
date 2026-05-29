34566b7118d60688b583977b1855a23bcc4f2dcf
lsantiago3 <36929397+lsantiago3@users.noreply.github.com>
2026-05-29 10:30:38 -0400

    Delete backend/app/api/api/v1/backend/app/api/api/v1/base.py

--- backend/app/api/api/v1/backend/app/api/api/v1/base.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-api_router = APIRouter()
-
-# Future routers will be added here:
-# from .user import router as user_router
-# api_router.include_router(user_router, prefix="/user", tags=["User"])
-
